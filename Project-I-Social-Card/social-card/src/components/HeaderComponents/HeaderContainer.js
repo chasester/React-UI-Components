7fb206056cfac2356fb42660c7027ef55111de96
Chase Wex <clapingmanly@hotmail.com>
2019-04-22 12:41:54 -0400

    Updated header file to spec

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+class HeaderContainer extends React.Component 
+{
+    render(){
+        var text = "Let's Learn Recat by building simple interfaces with componets. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using componets you are well on yoru way to mastering React.";
+        var imgurl = 'https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png';
+        return (
+            <div class="header-container">
+                    <ImageThumbnail imgscr={imgurl} />
+                    <div class="content-container">
+                        <HeaderTitle username="Lambda School" handle="LambdaSchool" date="April 22" />
+                        <HeaderContent content={text} />
+                    </div>
+            </div>
+        );
+    }
+    
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+class ImageThumbnail extends React.Component 
+{
+    render(){return(<img src={`${this.props.imgscr}`} alt="" width="10%" height="10%"></img>);}
+}
+
+export default ImageThumbnail;
