c6eecfd849fa5cb8e482dc809a0e36ebffc1f239
Chase Wex <clapingmanly@hotmail.com>
2019-04-22 16:33:17 -0400

    Created the display class to display the text given by the calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+class Display extends React.Component
+{
+    render()
+    {
+        return (<div className="display"><div className="text">{this.props.number}</div></div>);
+    }
+}
+
+export default Display;
