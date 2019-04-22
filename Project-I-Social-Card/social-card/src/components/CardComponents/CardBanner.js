60b7afc3c8c8921a0358ef96e2c6aeba3ced46f1
Chase Wex <clapingmanly@hotmail.com>
2019-04-22 13:31:26 -0400

    Added content to spec

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+
+class CardBanner extends React.Component 
+{
+    render(){return(<img src={`${this.props.imgscr}`} alt="" width="80%" height="80%"></img>);}
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+
+class CardContent extends React.Component 
+{
+    render()
+    {
+        return(<div class="content">
+            <strong><div class="">{this.props.title}</div></strong>
+            <p>{this.props.content}</p>
+            <a class="grayout" href={`https://www.${this.props.url}`} target="tab">{this.props.url}</a>
+        </div>)
+    }
+}
+
+export default CardContent;
