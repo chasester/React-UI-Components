d5963e112af46401016ca4fc0c8fe08b3480054f
Chase Wex <clapingmanly@hotmail.com>
2019-04-22 17:30:07 -0400

    added hover functionality to the footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,7 @@ class FooterContainer extends React.Component
 {
     render(){
         return (
-            <div class="footer-container" id="noselect">
+            <div class="footer-container grayout" id="noselect">
                     <span class="icon " onClick=""><i class="far fa-comment"></i> {this.props.comments ? this.props.comments : "" }</span>
                     <span class="icon " onClick=""><i class="fas fa-sync"></i> {this.props.reposts ? this.props.reposts : "" }</span>
                     <span class="icon " onClick=""><i class="far fa-heart"></i> {this.props.likes ? this.props.likes : "" }</span>
